d1fec2b63fe1d7268b7feb5ddba727c0b62a741d
renuaryanverma <104340533+renuaryanverma@users.noreply.github.com>
2022-10-11 18:36:50 +0530

    Create TokenRequest.java

---
+++ TokenRequest.java
@@ -0,0 +1,35 @@
+package com.project.demo.model.token;
+
+import java.io.Serializable;
+public class TokenRequest implements Serializable {
+	private static final long serialVersionUID = 5926468583005150707L;
+	private String username;
+	private String password;
+	
+	//default constructor for JSON Parsing
+	public TokenRequest()
+	{
+	}
+
+	public TokenRequest(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
