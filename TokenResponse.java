29d2980cb98973737d6b61302bddc49cd86c5291
renuaryanverma <104340533+renuaryanverma@users.noreply.github.com>
2022-10-11 18:37:29 +0530

    Create TokenResponse.java

---
+++ TokenResponse.java
@@ -0,0 +1,17 @@
+package com.project.demo.model.token;
+
+import java.io.Serializable;
+
+public class TokenResponse implements Serializable {
+
+	private static final long serialVersionUID = -8091879091924046844L;
+	private final String jwttoken;
+
+	public TokenResponse(String jwttoken) {
+		this.jwttoken = jwttoken;
+	}
+
+	public String getToken() {
+		return this.jwttoken;
+	}
+}
